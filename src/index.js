f21b8572a0d883aecfb53d42a6eecb02bd313f7e
Kusum Dhoundiyal <kusum.dhoundiyal96@gmail.com>
2017-10-04 17:04:03 +0530

    Applozic plugin in react

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { HashRouter, Route, Switch } from 'react-router-dom'
+import { createBrowserHistory } from 'history';
+
+// Containers
+import Full from './containers/Full/'
+
+// Views
+import Login from './views/Pages/Login/'
+
+const history = createBrowserHistory();
+
+ReactDOM.render((
+  <HashRouter history={history}>
+    <Switch>
+      <Route exact path="/login" name="Login Page" component={Login}/>
+      <Route path="/" name="Home" component={Full}/>
+    </Switch>
+  </HashRouter>
+), document.getElementById('root'))
